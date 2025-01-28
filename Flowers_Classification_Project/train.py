fc3894292bcaeeafa327e971268e93dff8520263
Glorius Musyawa <musyawaglorius06@gmail.com>
2025-01-28 12:13:52 +0300

    Update train.py
    Enabled my train.py to be dynamic to two pretrained models: vgg16,alexnet. Added functionalities for the user to check gpu availability and use it, determine where they will save their model's checkpoint, number of epochs and number of hidden units to use.

--- Flowers_Classification_Project/train.py
+++ Flowers_Classification_Project/train.py
@@ -1,17 +1,7 @@
-# Glorious Musyawa Project
-# Check torch version and CUDA status if GPU is enabled.
+# PROGRAMMER: GLORIUS MUSYAWA
+
+# Import necessary PyTorch libraries for building and training models
 import torch
-print(torch.__version__)
-print(torch.cuda.is_available())# Should return True when GPU is enabled. 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #Should return cuda if GPU is enabled 
-print(device)
-
-
-# Developing an AI application
-# 
-# Going forward, AI algorithms will be incorporated into more and more everyday applications. For example, you might want to include an image classifier in a smart phone app. To do this, you'd use a deep learning model trained on hundreds of thousands of images as part of the overall application architecture. A large part of software development in the future will be using these types of models as common parts of applications. 
-
-# Importing important pytorch libraries for computer vision
 from torch import nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
@@ -20,25 +10,64 @@ from torchvision import transforms
 from torchvision import datasets
 from torchvision import models
 
-# Importing important libraries for data pre-processing and visualizing
+# Import libraries for visualization and data handling
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-#Other necessary libraries
+# Other utility libraries
 import time
 import json
-
 from PIL import Image
 import requests
 import tarfile
 import os
 import shutil
+import argparse
 
 
-# ## Load the data
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Train a Flower Classifier")
+# gpu availability for use argument
+parser.add_argument('--gpu', action='store_true', help='Use GPU if available for training')
 
-# Download the dataset 
+# Model architecture argument (with choices)
+parser.add_argument('--arch', default='vgg16', choices=['vgg16', 'alexnet'], 
+                    help='Model architecture (default: vgg16)')
+
+# Directory to save the model checkpoint
+parser.add_argument('--save_dir', default='./', help='Directory to save checkpoints (default: current directory)')
+
+# Learning rate argument
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate (default: 0.0001)')
+
+# Number of hidden units for the classifier
+parser.add_argument('--hidden_units', type=int, default= 4096, help='Number of hidden units (default: 4096)')
+
+# Number of training epochs
+parser.add_argument('--epochs', type=int, default=23, help='Number of training epochs (default: 23)')
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Set device based on GPU availability
+if args.gpu and torch.cuda.is_available():
+    device = torch.device("cuda")  # Use GPU
+    print("Using GPU for training")
+else:
+    device = torch.device("cpu")  # Fall back to CPU
+    print("Using CPU for training")
+
+# Defining Input_features for the model chosen
+if args.arch == 'vgg16':
+    model = models.vgg16(pretrained = True)
+    in_features = 25088
+elif args.arch == "alexnet":
+    model = models.alexnet(pretrained = True)
+    in_features = 9216
+# Move the model to the appropriate device
+model.to(device)
+
+
 
 # Define the URL and folder paths
 url = "https://s3.amazonaws.com/content.udacity-data.com/nd089/flower_data.tar.gz"
@@ -81,21 +110,16 @@ if file_path.endswith("tar.gz"):
 os.remove(file_path)
 print(f"Removed the downloaded tar.gz file: {file_path}")
 
-# ## Data Description
-# The dataset is split into three parts, training, validation, and testing. For the training, you'll want to apply transformations such as random scaling, cropping, and flipping. This will help the network generalize leading to better performance. You'll also need to make sure the input data is resized to 224x224 pixels as required by the pre-trained networks.
-# 
-# The validation and testing sets are used to measure the model's performance on data it hasn't seen yet. For this you don't want any scaling or rotation transformations, but you'll need to resize then crop the images to the appropriate size.
-# 
-# The pre-trained networks you'll use were trained on the ImageNet dataset where each color channel was normalized separately. For all three sets you'll need to normalize the means and standard deviations of the images to what the network expects. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`, calculated from the ImageNet images.  These values will shift each color channel to be centered at 0 and range from -1 to 1.
-#  
+# Paths for data directories
 
 data_dir = 'flowers'
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
 
+
 # Defining transforms for the training, validation, and testing sets
-#Both Validation and test sets will undergo the same transforms 
+
 train_transforms = transforms.Compose([
     transforms.RandomRotation(30),
     transforms.RandomResizedCrop(224),
@@ -121,75 +145,54 @@ train_loader =DataLoader(train_dataset,batch_size = 32, shuffle= True,num_worker
 valid_loader = DataLoader(valid_dataset, batch_size = 32, shuffle = False, num_workers = 4)
 test_loader = DataLoader(test_dataset, shuffle = False )
 
-
-# Label mapping
-# 
-# You'll also need to load in a mapping from category label to category name. You can find this in the file `cat_to_name.json`. It's a JSON object which you can read in with the [`json` module](https://docs.python.org/2/library/json.html). This will give you a dictionary mapping the integer encoded categories to the actual names of the flowers.
+# Load class-to-name mapping
 
 with open('cat_to_name.json', 'r') as f:
     cat_to_name = json.load(f)
 
 
-# Building and training the classifier
-
-# Now that the data is ready, it's time to build and train the classifier. As usual, you should use one of the pretrained models from `torchvision.models` to get the image features. Build and train a new feed-forward classifier using those features.
-# 
-# ## Note for Workspace users: 
-# If your network is over 1 GB when saved as a checkpoint, there might be issues with saving backups in your workspace. Typically this happens with wide dense layers after the convolutional layers. If your saved checkpoint is larger than 1 GB (you can open a terminal and check with `ls -lh`), you should reduce the size of your hidden layers and train again.
-
-# Building and training the network
-#Using Vgg16 as my backbone(pre-trained) network
-#Loading the pre-trained model
-vgg16 = models.vgg16(pretrained = True)
-
-#Freezing the pre-trained model's parameters
-#Why?- So that only the feedforward layers will be updated during training
-
-for param in vgg16.features.parameters():
-    param.requires_grad = False
-
-
-
-
 #Defining the classifier network
 class FlowerClassifier(nn.Module):
-    def __init__(self,output_size):
+    def __init__(self,base_model,output_size,in_features,hidden_units):
         super(FlowerClassifier,self).__init__()
-        self.vgg16 = models.vgg16(pretrained = True)
-        self.vgg16.features.requires_grad = False
-        
-#Creating a new classifier        
-        
+        self.base_model = base_model
+        self.in_features = in_features
+
+        # Freeze base model parameters
+        for param in base_model.features.parameters():
+            param.requires_grad = False
+            
+        #Creating a new classifier        
         self.classifier = nn.Sequential(
-            nn.Linear(25088,4096), #Starting from vgg16's output feature layer
+            nn.Linear(in_features,hidden_units), 
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.Linear(4096,1024),
+            nn.Linear(hidden_units,1024),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.Linear(1024,102)
         )
     
     def forward(self,x):
-        x = self.vgg16.features(x)
-        x = x.view(-1,25088)
+        x = self.base_model.features(x)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x   
 
-#instantiating the model
-model = FlowerClassifier(102) # The dataset has 102 classes
 
-#Use GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#Initializing the model
+
+output_size = 102
+# Use specified hidden units 
+hidden_units = args.hidden_units
+
+model = FlowerClassifier(model,output_size = output_size,in_features = in_features,hidden_units = hidden_units) # The dataset has 102 classes
+
 model.to(device)
-
 
 #Defining the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.classifier.parameters(),lr=0.0001)
-
-#Defining the training loop
-#I intent to time the training
+optimizer = torch.optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
 
 #Defining the training function
 def train_model(model,criterion,optimizer,num_epochs = 23):
@@ -255,13 +258,9 @@ def train_model(model,criterion,optimizer,num_epochs = 23):
     print(f'Best val Acc: {best_acc:.4f}')
 
 # Train the model
-train_model(model, criterion, optimizer, num_epochs=23)
+train_model(model, criterion, optimizer, num_epochs=args.epochs)
         
 
-
-# ## Testing your network
-# 
-# It's good practice to test your trained network on test data, images the network has never seen either in training or validation. This will give you a good estimate for the model's performance on completely new images. Run the test images through the network and measure the accuracy, the same way you did validation. You should be able to reach around 70% accuracy on the test set if the model has been trained well.
 
 # Evaluating the model
 def test_model(model, dataloader):
@@ -286,23 +285,14 @@ def test_model(model, dataloader):
     print(f'Test Loss: {test_loss:.4f}')
     print(f'Test Accuracy: {test_acc:.4f}')
 
-
-
 # Evaluate the trained model on the test dataset
 print("Evaluating the model on the test dataset:")
 test_model(model, test_loader)
 
 
-# Save the checkpoint
-# 
-# Now that your network is trained, save the model so you can load it later for making predictions. You probably want to save other things such as the mapping of classes to indices which you get from one of the image datasets: `image_datasets['train'].class_to_idx`. You can attach this to the model as an attribute which makes inference easier later on.
-# 
-# ```model.class_to_idx = image_datasets['train'].class_to_idx```
-# 
-# Remember that you'll want to completely rebuild the model later so you can use it for inference. Make sure to include any information you need in the checkpoint. If you want to load the model and keep training, you'll want to save the number of epochs as well as the optimizer state, `optimizer.state_dict`. You'll likely want to use this trained model in the next part of the project, so best to save it now.
 
 # Saving the model checkpoint
- # Attach class-to-index mapping
+# Attach class-to-index mapping
 model.class_to_idx = train_dataset.class_to_idx
 
 # Create the checkpoint dictionary
@@ -311,13 +301,16 @@ checkpoint = {
     "class_to_idx": model.class_to_idx,
     "optimizer_state_dict": optimizer.state_dict(),
     "epochs": 23,
-    "learning_rate": 0.0001,
-    "architecture": "vgg16",
+    "learning_rate": optimizer.param_groups[0]['lr'],
+    "architecture": args.arch,
     "classifier": model.classifier,
+    "in_features": in_features,
+    "hidden_units": hidden_units,
     "output_size": 102
 }
 
 # Save the checkpoint to a file
-torch.save(checkpoint, "flower_classifier_checkpoint.pth")
+checkpoint_name = f"{args.save_dir}/flower_classifier_checkpoint_{args.arch}.pth"
+torch.save(checkpoint, checkpoint_name)
 
-print("The model's checkpoint is saved as 'flower_classifier_checkpoint.pth'.")
+print(f"The model's checkpoint is saved as '{checkpoint_name}'.")
